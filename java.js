98f6cd7b299d04253b0b7525804331e4fbd015c0
tenzin kunkyab <tenzinkunkyab5@gmail.com>
2023-08-08 00:00:43 -0400

    added code in java.js

--- java.js
+++ java.js
@@ -1,13 +1,93 @@
-function playRound(playerSelection, computerSelection) {
-    // your code here!
+ let playerScore = 0;
+ let computerScore = 0;
+ let roundWinner;
+
+
+
+
+function playRound1(playerSelection, computerSelection) 
+{
+     if(playerSelection =="scissor" && computerSelection =="rock")
+     {
+        roundWinner= "You lose! rock beats scissor";
+        computerScore++;
+     } 
+     else if (playerSelection =="rock" && computerSelection =="paper")
+     {
+        roundWinner= "You lose! paper beats rock";
+     }
+     else if(playerSelection=="paper" && computerSelection=="scissor")
+     {
+     roundWinner= "you lose! scissor cuts paper" ;
+     computerScore++;
   }
-   
-function getComputerChoice(){
-    const choices = ["Rock","paper","scissor"];
-    const random_choices =   Math.floor(Math.random()*choices.length);
-    console.log(random_choices);
+
+  else if (playerSelection =="rock"&& computerSelection =="scissor")
+  {
+    roundWinner="You win! rock beats scissor";
+     playerScore++;
+  } 
+
+  else if(playerSelection =="scissor" && computerSelection =="paper")
+  {
+   roundWinner="You win! scissor cuts paper";
+    playerScore++;
+  }
+else if(playerSelection =="paper" && computerSelection=="rock") 
+{
+    roundWinner= "You win! paper beats rock";
+    playerScore++;
+}
+else{
+    roundWinner = "its tie";
+}
+return roundWinner;
+return playerScore;
+
 }
 
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+
+
+
+function getComputerChoice(){
+    const choices = ["rock","paper","scissor"];
+    const random_choices =  choices[ Math.floor(Math.random()*choices.length)];
+     console.log(random_choices);
+    return random_choices;
+}
+
+function playRound() {
+    const computerSelection = getComputerChoice();
+    const  playerSelection = prompt(("enter your choices: ").toLowerCase());
+    console.log(playerSelection);
+   
+    const winner = playRound1(playerSelection, computerSelection);
+    console.log(winner);
+    
+    
+}
+
+
+function game()
+ {
+    for (let i = 0; i <=5; i++) {
+        playRound();
+
+        }
+    
+  
+}
+
+
+ 
+
+  //console.log(playerSelection);
+ 
+ game();
+//  const p = playRound(playerSelection,computerSelection);
+// console.log(p);
+
+
+ 
+
+  
